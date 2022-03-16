a575f2b94b32a00322aed68368e3a60c2d662c29
suresh <suppada6311@ucumberlands.edu>
2022-03-16 15:37:10 -0400

    updating user-ansible.sh

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 // Provider specific configs
 module "my_ec2" {
   source              = "git::https://github.com/suppada/terraform.git//modules/ec2"
-  ec2_count           = "0"
+  ec2_count           = "2"
   ami_id              = "ami-0b0af3577fe5e3532"
   instance_type       = "t2.micro"
   key_name            = "suresh"
-  role_name           = "test-java"
+  role_name           = "java-test"
   user_data           = file("user-ansible.sh")
-  security_group_name = "test-java"
-  instance_profile    = "test-java"
-  role_policy         = "test-java"
+  security_group_name = "java-test"
+  instance_profile    = "java-test"
+  role_policy         = "java-test"
   aws_region          = "us-east-1"
   subnet_id           = "subnet-06909708"
   environment_tag     = "dev"
