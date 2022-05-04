44c5f3d8afa7e8d7fa40f23e618715631a2261df
suppada <suppada6311@ucumberlands.edu>
2022-05-04 16:39:29 -0400

    updating volume size in main.tf file

--- main.tf
+++ main.tf
@@ -1,19 +1,20 @@
 // Provider specific configs
-module "my_ec2" {
+module "master" {
   source              = "git::https://github.com/suppada/terraform.git//modules/ec2"
-  ec2_count           = "2"
+  ec2_count           = "1"
   ami_id              = "ami-0b0af3577fe5e3532"
   instance_type       = "t2.micro"
   key_name            = "suresh"
-  role_name           = "java-test"
-  user_data           = file("install.sh")
-  security_group_name = "java-test"
-  instance_profile    = "java-test"
-  role_policy         = "java-test"
+  user_data           = file("bash.sh")
   aws_region          = "us-east-1"
   subnet_id           = "subnet-06909708"
-  environment_tag     = "dev"
-  instance_name       = "Ansible"
+  environment_tag     = "Dev"
+  instance_name       = "master"
+  project             = "Devops"
   owner               = "suresh"
-  project             = "Automation"
+  role_name           = "dev"
+  role_policy         = "dev"
+  security_group_name = "dev"
+  volume_size         = "30"
+  instance_profile    = "dev"
 }
